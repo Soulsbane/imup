8fc5c67361c2a78001bcbb86b7af9154a9f2847c
Paul Crane <paul@acheronsoft.com>
2020-10-27 03:23:23 -0500

    Add fetchPage function.

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Soulsbane/imup
 
 go 1.15
 
-require github.com/imroc/req v0.3.0 // indirect
+require github.com/imroc/req v0.3.0
--- imup.go
+++ imup.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/imroc/req"
+)
+
+func fetchPage(name string) bool {
+
+	req.SetTimeout(5 * time.Second)
+	_, err := req.Get(name)
+
+	if err != nil {
+		//log.Fatal(err)
+		fmt.Println("Reached timeout or error. Your internet is probably down!")
+		return false
+	}
+
+	fmt.Println("Your internet is working!")
+	return true
+}
+
+func main() {
+	fetchPage("http://google.com/")
 }

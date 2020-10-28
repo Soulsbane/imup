db43af142b4edeeb4431e8d4e0c1dabc680f529d
Paul Crane <paul@acheronsoft.com>
2020-10-28 02:18:14 -0500

    Option to repeat fetching of webpage.

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/Soulsbane/imup
 
 go 1.15
 
-require github.com/imroc/req v0.3.0
+require (
+	github.com/alexflint/go-arg v1.3.0
+	github.com/imroc/req v0.3.0
+	github.com/roylee0704/gron v0.0.0-20160621042432-e78485adab46
+)
--- imup.go
+++ imup.go
@@ -5,15 +5,24 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/alexflint/go-arg"
 	"github.com/imroc/req"
 )
 
 func getRandomSecond() int {
 	rand.Seed(time.Now().UnixNano())
-	min := 10
+	min := 30
 	max := 60
 
 	return rand.Intn(max-min+1) + min
+}
+
+func keepFetchingPage(name string) {
+	for {
+		second := getRandomSecond()
+		fetchPage(name)
+		time.Sleep(time.Duration(second) * time.Second)
+	}
 }
 
 func fetchPage(name string) bool {
@@ -21,6 +30,7 @@ func fetchPage(name string) bool {
 	req.SetTimeout(5 * time.Second)
 	_, err := req.Get(name)
 
+	// TODO: Add time logging.
 	if err != nil {
 		fmt.Println("Reached timeout or error. Your internet is probably down!")
 		return false
@@ -31,5 +41,15 @@ func fetchPage(name string) bool {
 }
 
 func main() {
-	fetchPage("http://google.com/")
+	var args struct {
+		Repeat bool `arg:"-r, --repeat" default:"false" help:"Keep checking periodically."`
+	}
+
+	arg.MustParse(&args)
+
+	if args.Repeat {
+		keepFetchingPage("http://google.com/")
+	} else {
+		fetchPage("http://google.com/")
+	}
 }

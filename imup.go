01433e512b15d396b2d53b6831d44589f1e94fe7
Paul Crane <paul@acheronsoft.com>
2020-11-01 03:11:15 -0600

    Fix minor formatting issue.

--- imup.go
+++ imup.go
@@ -35,18 +35,19 @@ func keepFetchingPage(name string) {
 }
 
 func fetchPage(name string) bool {
-
 	req.SetTimeout(5 * time.Second)
 	_, err := req.Get(name)
 
 	if err != nil {
 		fmt.Print(getFormattedTime())
 		fmt.Println(" - Reached timeout or error. Your internet is probably down!")
+
 		return false
 	}
 
 	fmt.Print(getFormattedTime())
 	fmt.Println(" - Your internet is working!")
+
 	return true
 }
 
